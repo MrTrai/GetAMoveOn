9687fe4cd0e14de54a86db38a046773ebf37400c
Kiet Tran <K@lawn-128-61-120-6.lawn.gatech.edu>
2018-03-03 08:44:40 -0500

    change

--- aceso-ui/src/app/login/login.component.ts
+++ aceso-ui/src/app/login/login.component.ts
@@ -1,15 +1,40 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from "@angular/router";
+import { Observable } from 'rxjs/Observable';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    moduleId: module.id,
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
+    loginData = { username:'', password:'' };
+    message = '';
+    data: any;
 
-  constructor() { }
+    constructor(private http: HttpClient, private router: Router) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
 
+    }
+
+    login() {
+      this.http.post('/api/signin',this.loginData).subscribe(resp => {
+        this.data = resp;
+        localStorage.setItem('jwtToken', this.data.token);
+        this.router.navigate(['books']);
+      }, err => {
+        this.message = err.error.msg;
+      });
+    }
 }
